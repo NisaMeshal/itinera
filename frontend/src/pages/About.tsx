7267a13309908219d8fd084b273c3ce9efd4804d
NisaMeshal <nisameshal@gmail.com>
2025-06-14 15:11:04 -0400

    updated gitignore

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { PageWrapper } from "../components/PageWrapper";
 import { Text } from '@mantine/core';
 import { Link } from 'react-router-dom';
 
 export default function About() {
     return (
-        <PageWrapper>
-            <Text>Hello world!</Text>
-        </PageWrapper>
+        <Text>Hello world!</Text>
     );
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { PageWrapper } from "../components/PageWrapper";
 import { Text } from '@mantine/core';
 import { Link } from 'react-router-dom';
 import NavBar from "../components/NavBar";
 
 export default function Home() {
     return (
-        <PageWrapper>
-            <NavBar></NavBar>
-        </PageWrapper>
+        <NavBar></NavBar>
     );
 }
